8d628c656f8b1fffea019ca52535f790ad8edf84
buiphuochai3010 <buiphuochai3010@gmail.com>
2025-04-29 22:49:26 +0700

    Refactor app.controller.ts constructor spacing, update Next.js config to disable strict mode and set environment PORT, and modify package.json scripts to specify port 4002 for dev and start commands.

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  env: {
+    PORT: process.env.PORT,
+  },
 };
 
 export default nextConfig;
